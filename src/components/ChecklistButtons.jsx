1516e4d7918995f64cc15e6bebc22bb0eb701b2d
miawithcode <cuttontail@gmail.com>
2024-06-02 18:53:51 +0800

    refactor: map buttons

--- src/components/ChecklistButtons.jsx
+++ src/components/ChecklistButtons.jsx
@@ -4,21 +4,21 @@ import CheckmarkCircle02Icon from './icons/CheckmarkCircle02Icon';
 import Delete02Icon from './icons/Delete02Icon';
 import RemoveCircleIcon from './icons/RemoveCircleIcon';
 
+const checklistButtons = [
+  { text: 'Mark all as complete', icon: <CheckmarkCircle02Icon /> },
+  { text: 'Mark all as incomplete', icon: <RemoveCircleIcon /> },
+  { text: 'Reset to initial', icon: <ArrowReloadHorizontalIcon /> },
+  { text: 'Remove all items', icon: <Delete02Icon /> },
+];
+
 const ChecklistButtons = () => {
   return (
     <section className="btn-container">
-      <Button>
-        <CheckmarkCircle02Icon /> Mark all as complete
-      </Button>
-      <Button>
-        <RemoveCircleIcon /> Mark all as incomplete
-      </Button>
-      <Button>
-        <ArrowReloadHorizontalIcon /> Reset to initial
-      </Button>
-      <Button>
-        <Delete02Icon /> Remove all items
-      </Button>
+      {checklistButtons.map((button) => (
+        <Button key={button.text}>
+          {button.icon} {button.text}
+        </Button>
+      ))}
     </section>
   );
 };
--- src/components/GeneralButtons.jsx
+++ src/components/GeneralButtons.jsx
@@ -2,15 +2,19 @@ import Button from './Button';
 import AddCircleIcon from './icons/AddCircleIcon';
 import ArrowDataTransferVerticalIcon from './icons/ArrowDataTransferVerticalIcon';
 
+const generalButtons = [
+  { text: 'New item', icon: <AddCircleIcon /> },
+  { text: 'Sort by', icon: <ArrowDataTransferVerticalIcon /> },
+];
+
 const GeneralButtons = () => {
   return (
     <section className="btn-container">
-      <Button>
-        <AddCircleIcon /> New item
-      </Button>
-      <Button>
-        <ArrowDataTransferVerticalIcon /> Sort by
-      </Button>
+      {generalButtons.map((button) => (
+        <Button key={button.text}>
+          {button.icon} {button.text}
+        </Button>
+      ))}
     </section>
   );
 };
