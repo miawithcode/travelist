63f4e6dc02c2488c02b6c7eeb4f1d354bc1971ee
miawithcode <cuttontail@gmail.com>
2024-06-05 01:11:31 +0800

    chore: update initialItems

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -2,20 +2,15 @@ export const initialItems = [
   {
     category: 'Important',
     categoryItems: [
-      { id: 1, label: 'Good mood', isPacked: false },
+      { id: 1, label: 'Good mood', isPacked: true },
       { id: 2, label: 'Passport', isPacked: false },
       { id: 3, label: 'Credit card', isPacked: false },
     ],
     color: '#f9eaeb',
   },
   {
-    category: 'Toiletries',
-    categoryItems: [
-      { id: 4, label: 'Toothbrush', isPacked: false },
-      { id: 6, label: 'Toothpaste', isPacked: false },
-      { id: 7, label: 'Razor', isPacked: false },
-      { id: 8, label: 'Nail clipper', isPacked: false },
-    ],
+    category: 'Digital Products',
+    categoryItems: [{ id: 4, label: 'Phone charger', isPacked: false }],
     color: '#eaf4f9',
   },
 ];
